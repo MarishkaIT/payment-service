14e9c8c27a1cebc3e7ce3c83d9b27c78c19db1fe
marishka_rish <litvinskaya.rish@gmail.com>
2024-08-20 15:08:58 +0300

    refactoring

--- src/main/java/com/example/paymentservice/entity/Payment.java
+++ src/main/java/com/example/paymentservice/entity/Payment.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
@@ -16,14 +13,21 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "payments")
 public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(precision = 19, scale = 2)
     private BigDecimal amount;
+    @Column(length = 3)
     private String currency;
+    @Enumerated(EnumType.STRING)
     private PaymentMethod paymentMethod;
+    @Enumerated(EnumType.STRING)
     private PaymentStatus status;
 }
